e7019fb2299b29e639b41ccb4391eeac5da42b72
SouleymaneSy7 <souleymanesyservices@gmail.com>
2026-07-29 08:07:41 +0000

    feat(imports): add barrel files for common, layout, shared, ui, hooks, services, store, utils directories

---
+++ src/components/common/index.ts
@@ -0,0 +1,7 @@
+export { default as Container } from "./container";
+export * from "./icon";
+export { default as KeyboardShortcuts } from "./keyboard-shortcuts";
+export { default as List } from "./list";
+export { default as LoadingStatus } from "./loading-status";
+export { default as Title } from "./title";
+export { default as VisuallyHidden } from "./visually-hidden";
---
+++ src/components/layout/index.ts
@@ -0,0 +1,6 @@
+export { default as Footer } from "./footer";
+export { default as Header } from "./header";
+export { default as Main } from "./main";
+export { default as Navbar } from "./navbar";
+export { default as TabNav } from "./tab-nav";
+export { default as ThemeToggle } from "./theme-toggle";
